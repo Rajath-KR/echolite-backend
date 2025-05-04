d4b81c3ce4a42825c60b21c7430f368a51f0d138
Rajath-KR <161421731+Rajath-KR@users.noreply.github.com>
2025-05-04 15:49:09 +0530

    Add files via upload

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,18 @@
+
 import { signup, signin } from '../controllers/user.js';
 import express from 'express';
+import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
 
 router.post('/signup', signup);
-
 router.post('/signin', signin);
+
+router.get('/me', protect, (req, res) => {
+  res.json({
+    message: 'Protected route accessed successfully',
+    user: req.user
+  });
+});
 
 export default router;
