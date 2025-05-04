d393c0b54565089d917a4559a9a7ef5cd650013e
Rajath-KR <161421731+Rajath-KR@users.noreply.github.com>
2025-05-04 15:48:29 +0530

    Add files via upload

--- models/userModal.js
+++ models/userModal.js
@@ -1,4 +1,6 @@
+
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -28,6 +30,26 @@ const userSchema = new mongoose.Schema({
     ref: 'User'
   }]
 }, { timestamps: true });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 const User = mongoose.model('User', userSchema);
 export default User;
